c40e237a60530c881c7bd20c9a7e1d6fd861e280
alvinary <alvaro.cuestas.ll@gmail.com>
2022-01-11 15:35:28 -0300

    Substitute ' \n' with '\n' in preprocessing, add Parser.parse() draft

--- parser.py
+++ parser.py
@@ -1,11 +1,31 @@
-import models
-from lark import Lark
+from lark import Lark, Tree, Token
 
 grammar = ""
 
 with open("grammar") as grammar_file:
     for line in grammar_file:
         grammar = grammar + line
+
+def get_preterminal(lark_tree):
+    return lark_tree.data[0:]
+
+def get_rules(program_tree):
+    pass
+
+def get_sort_declarations(program_tree):
+    pass
+
+def get_head(rule_tree):
+    pass
+
+def get_body(rule_tree):
+    pass
+
+def get_atoms(atoms_tree):
+    pass
+
+def get_leaves(lark_tree):
+    pass
 
 class Parser:
 
@@ -17,6 +37,9 @@ class Parser:
 
         program = program.replace("=>", " => ")
         program = program.replace(",", ", ")
+
+        while " \n" in program:
+            program = program.replace(" \n", "\n")
 
         while "  " in program:
             program = program.replace("  ", " ")
@@ -34,7 +57,23 @@ class Parser:
 
         return "\n".join(parts)
 
-    def get_solver(self, program):
+    def parse(self, program):
+        program = self.prepocess(program)
+        parsed_program = self.parser.parse(program)
+        statements = [s for s in parsed_program.children if isinstance(s, Tree)]
+        sorts = [s for s in statements if get_preterminal(s) == "sort"]
+        rules = [s for s in statements if get_preterminal(s) == "rule"]
+
+    def declutter_tree(self, tree):
+
+        if isinstance(tree, Tree):
+            if get_preterminal(tree) == "sort":
+                return 
+        
+        if isinstance(tree, Token):
+            pass
+
+    def get_rules(self, program):
 
         return "Je"
         
