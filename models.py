7d13d53f3ce65e285b25597109a1ab60757e26eb
alvinary <alvaro.cuestas.ll@gmail.com>
2022-01-20 18:54:58 -0300

    Fix bugs and typos in models.py
    - Add a TERM_SEPARATOR variable
    - Redefine Rule.replace() to work with terms having function applications
    - Add HornSolver.dimacs(), formerly called "clausify"
    - Redefine Relation.get_string_encoding() using TERM_SEPARATOR
    - Fix typo in HornSolver.evaluate() - t.strip was replaced with t.strip()
    - HornSolver.evaluate() now returns a whole atom as a string, instead of a
    single evaluated term
    - Substitute 'new_clause' (which was undefined) with 'function_free_clause'
    in  HornSolver.evaluate_functions()
    
    - Add several debugging print statements

--- models.py
+++ models.py
@@ -6,6 +6,8 @@ from copy import copy
 
 from pysat.solvers import Glucose3
 
+TERM_SEPARATOR = "--"
+
 def index():
     return defaultdict(lambda: [])
 
@@ -14,7 +16,7 @@ class Relation:
     parts: List[str]
 
     def get_string_encoding(self):
-        return " ".join([p.strip() for p in self.parts])
+        return TERM_SEPARATOR.join([p.strip() for p in self.parts])
 
 
 @dataclass
@@ -40,6 +42,9 @@ class Rule:
         heads = [self.bind_variables(r) for r in self.heads]
         body = [self.bind_variables(r) for r in self.body]
 
+        print(heads)
+        print(body)
+
         string_heads = [r.get_string_encoding() for r in heads]
         string_body = [r.get_string_encoding() for r in body]
         if string_heads:
@@ -56,10 +61,15 @@ class Rule:
         return Relation([self.replace(s) for s in relation.parts])
 
     def replace(self, symbol):
-        if symbol in self.bindings:
-            return self.bindings[symbol]
-        else:
-            return symbol
+        if "." not in symbol:
+            if symbol in self.bindings:
+                return self.bindings[symbol]
+            else:
+                print(f"symbol {symbol} not in {self.bindings.keys()}")
+                return symbol
+        if "." in symbol:
+            symbol_parts = [p.strip() for p in symbol.split(".")]
+            return ".".join([self.replace(s) for s in symbol_parts])
 
 
 class HornSolver:
@@ -85,40 +95,74 @@ class HornSolver:
     def unfold_instance(self):
         
         for rule in self.rules:
+
+            print(rule.body)
+            print(rule.sorts)
             
             for assignment in product(*[self.sorts[s] for s in rule.sorts]):
                 
+                print(assignment)
+
                 clauses = rule.get_clauses(assignment)
                 
+                for c in clauses:
+                    print("clause:")
+                    print(f"   head: {c.head}")
+                    print(f"   body: {', '.join([b for b in c.body])}")
+
                 pure_clauses = [self.evaluate_functions(c) for c in clauses]
                 
                 self.update_maps(pure_clauses)
 
-                cnf_clauses = self.clausify(pure_clauses)
+                cnf_clauses = [self.dimacs(c) for c in pure_clauses]
 
                 for cnf_clause in cnf_clauses:
                     self.solver.add_clause(cnf_clause)
 
     def is_functional(self, term_string):
         return "." in term_string
+
+    def dimacs(self, pure_clause):
+
+        dimacs_clause = []
+
+        if pure_clause.head != "":
+            dimacs_clause.append(self.literal_map[pure_clause.head])
+
+        for atom in pure_clause.body:
+            dimacs_clause.append(-self.literal_map[atom])
+
+        return dimacs_clause
+        
         
     def evaluate(self, term_string):
+
+        terms = term_string.split(TERM_SEPARATOR)
+        evaluated_terms = []
+
+        for term in terms:
         
-        parts = [t.strip for t in term_string.split(".")]
-        domain = parts.pop(0)
-        image = domain
+            parts = [t.strip() for t in term.split(".")]
+            
+            domain = parts.pop(0)
 
-        while parts:
-            image = self.value_map[domain, parts.pop(0)]
-            domain = image
+            image = domain
 
-        return image
+            while parts:
+
+                image = self.value_map[domain, parts.pop(0)]
+
+                domain = image
+
+            evaluated_terms.append(image)
+
+        return " ".join(evaluated_terms)
 
     def evaluate_functions(self, clause):
 
         function_free_clause = copy(clause)
-        function_free_clause.head = self.evaluate(new_clause.head)
-        function_free_clause.body = [self.evaluate(atom) for atom in new_clause.body]
+        function_free_clause.head = self.evaluate(function_free_clause.head)
+        function_free_clause.body = [self.evaluate(atom) for atom in function_free_clause.body]
             
         return function_free_clause
 
