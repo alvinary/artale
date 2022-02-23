ac995bd20ff2e2b6afbad3e7d8a84a2b8e2721b7
alvinary <alvaro.cuestas.ll@gmail.com>
2022-02-23 20:42:36 -0300

    Handle interpretation of disjunctions

--- models.py
+++ models.py
@@ -4,12 +4,14 @@ from itertools import product
 from dataclasses import dataclass
 from copy import copy
 
-from pysat.solvers import Glucose3
+from pysat.solvers import Solver
 
 TERM_SEPARATOR = "--"
+IS_DISJUNCTION = "vee"
 
 def index():
     return defaultdict(lambda: [])
+
 
 @dataclass
 class Relation:
@@ -17,7 +19,6 @@ class Relation:
 
     def get_string_encoding(self):
         return TERM_SEPARATOR.join([p.strip() for p in self.parts])
-
 
 @dataclass
 class Clause:
@@ -35,8 +36,18 @@ class Rule:
 
     flags: Set[str]
 
-    def get_clauses(self, assignment):
+    def get_disjunction(self, assignment):
         
+        self.rebind(self, assignment)
+
+        disjuncts = [self.bind_variables(d) for d in self.heads]
+
+        string_disjuncts = [d.get_string_encoding() for d in disjuncts]
+
+        return [Disjunction(disjuncts)]
+
+    def get_clauses(self, assignment):
+
         self.rebind(assignment)
 
         heads = [self.bind_variables(r) for r in self.heads]
@@ -59,17 +70,17 @@ class Rule:
         return Relation([self.replace(s) for s in relation.parts])
 
     def replace(self, symbol):
-        
+
         if "." not in symbol:
-            
+
             if symbol in self.bindings:
                 return self.bindings[symbol]
-            
+
             else:
                 return symbol
-        
+
         if "." in symbol:
-        
+
             symbol_parts = [p.strip() for p in symbol.split(".")]
             return ".".join([self.replace(s) for s in symbol_parts])
 
@@ -85,7 +96,7 @@ class HornSolver:
         self.value_map = {}
         self.literal_map = {}
         self.reverse_literal_map = {}
-        self.solver = Glucose3()
+        self.solver = Solver()
         self.name_counter = 0
 
     def reset_maps(self):
@@ -95,18 +106,22 @@ class HornSolver:
         self.value_map = {}
 
     def unfold_instance(self):
-        
+
         for rule in self.rules:
-            
+
             for assignment in product(*[self.sorts[s] for s in rule.sorts]):
 
                 clauses = rule.get_clauses(assignment)
 
                 pure_clauses = [self.evaluate_functions(c) for c in clauses]
-                
+
                 self.update_maps(pure_clauses)
 
-                cnf_clauses = [self.dimacs(c) for c in pure_clauses]
+                if IS_DISJUNCTION in rule.flags:
+                    cnf_clauses = [self.disjunction_dimacs(c) for c in pure_clauses]
+
+                else:
+                    cnf_clauses = [self.dimacs(c) for c in pure_clauses]
 
                 for cnf_clause in cnf_clauses:
                     self.solver.add_clause(cnf_clause)
@@ -125,17 +140,19 @@ class HornSolver:
             dimacs_clause.append(-self.literal_map[atom])
 
         return dimacs_clause
-        
-        
+
+    def disjunction_dimacs(self, pure_clause):
+        return [self.literal_map[atom] for atom in pure_clause.body]
+
     def evaluate(self, term_string):
 
         terms = term_string.split(TERM_SEPARATOR)
         evaluated_terms = []
 
         for term in terms:
-        
+
             parts = [t.strip() for t in term.split(".")]
-            
+
             domain = parts.pop(0)
 
             image = domain
@@ -154,18 +171,20 @@ class HornSolver:
 
         function_free_clause = copy(clause)
         function_free_clause.head = self.evaluate(function_free_clause.head)
-        function_free_clause.body = [self.evaluate(atom) for atom in function_free_clause.body]
-            
+        function_free_clause.body = [
+            self.evaluate(atom) for atom in function_free_clause.body
+        ]
+
         return function_free_clause
 
     def update_maps(self, clauses):
-        
+
         all_symbols = set()
 
         for c in clauses:
             all_symbols.add(c.head)
             all_symbols |= set(c.body)
-        
+
         new_symbols = {s for s in all_symbols if s not in self.literal_map}
 
         for s in new_symbols:
