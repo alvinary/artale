3f21f4f73be983ab631df4f73313a6a82fc3809b
alvinary <alvaro.cuestas.ll@gmail.com>
2022-02-25 12:53:24 -0300

    Add tile tagger

---
+++ ttag.py
@@ -0,0 +1,261 @@
+from collections import defaultdict
+
+import pyglet
+from pyglet.gl import glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_TEXTURE_MIN_FILTER, GL_NEAREST
+
+SCROLL_SCALING = 10
+TILE_SIDE = 64
+SCROLLABLE_PANEL_TOP = 400
+TAG_SEPARATOR = 8
+TAG_HEIGHT = 16
+
+alphabet = {}
+alphabet['a'] = pyglet.window.key.A
+alphabet['b'] = pyglet.window.key.B
+alphabet['c'] = pyglet.window.key.C
+alphabet['d'] = pyglet.window.key.D
+alphabet['e'] = pyglet.window.key.E
+alphabet['f'] = pyglet.window.key.F
+alphabet['g'] = pyglet.window.key.G
+alphabet['h'] = pyglet.window.key.H
+alphabet['i'] = pyglet.window.key.I
+alphabet['j'] = pyglet.window.key.J
+alphabet['k'] = pyglet.window.key.K
+alphabet['l'] = pyglet.window.key.L
+alphabet['m'] = pyglet.window.key.M
+alphabet['n'] = pyglet.window.key.N
+alphabet['o'] = pyglet.window.key.O
+alphabet['p'] = pyglet.window.key.P
+alphabet['q'] = pyglet.window.key.Q
+alphabet['r'] = pyglet.window.key.R
+alphabet['s'] = pyglet.window.key.S
+alphabet['t'] = pyglet.window.key.T
+alphabet['u'] = pyglet.window.key.U
+alphabet['v'] = pyglet.window.key.V
+alphabet['w'] = pyglet.window.key.W
+alphabet['x'] = pyglet.window.key.X
+alphabet['y'] = pyglet.window.key.Y
+alphabet['z'] = pyglet.window.key.Z
+
+letters = [alphabet[i] for i in "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()]
+
+cool_purple = (58, 58, 165)
+cool_coral = (192, 64, 64)
+cool_white = (248, 231, 231)
+
+window = pyglet.window.Window()
+
+background_batch = pyglet.graphics.Batch()
+foreground_batch = pyglet.graphics.Batch()
+hover_batch = pyglet.graphics.Batch()
+
+color_batch = pyglet.graphics.Batch()
+
+index = lambda: defaultdict(lambda : set())
+
+tileset_resource = pyglet.image.load("./images/cave.png")
+
+class TileTagger:
+    def __init__(self):
+        self.properties = set()
+        self.tile_index = index() # tile_index[a, b] = tile_name
+        self.property_index = index()
+        self.tilemap = pyglet.sprite.Sprite(tileset_resource, x=0, y=0, batch=background_batch)
+        self.panel = None
+        self.label = None
+        self.tile_x = 0
+        self.tile_y = 0
+        self.scroll_shift_y = 0
+        self.scroll_shift_x = 0
+        
+        self.tilemap.scale_x = 4
+        self.tilemap.scale_y = 4
+    
+    def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
+        self.tilemap.y = self.tilemap.y - int(scroll_y * SCROLL_SCALING)
+        self.scroll_shift_y -= int(scroll_y * SCROLL_SCALING)
+
+    def on_key_press(self, symbol, modifiers):
+
+        if symbol == pyglet.window.key.LSHIFT:
+            if self.property_index[self.tile_x, self.tile_y]:
+                self.set_panel(self.mouse_x, self.mouse_y)
+                
+        if symbol == pyglet.window.key.TAB:
+            self.dump_string()
+        
+    def on_mouse_press(self, x, y, button, modifiers):
+        
+        if button == pyglet.window.mouse.LEFT:
+
+            print(f"x: {self.tile_x}, y: {self.tile_y}")
+
+            if self.label:
+                if self.label.label_item:
+                    self.label.label_item.delete()
+                self.label = None
+
+            self.label = ShortTextInput(self, ">", x, y)
+            window.push_handlers(self.label)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.tile_x = (x - self.scroll_shift_x) // TILE_SIDE
+        self.tile_y = (y - self.scroll_shift_y) // TILE_SIDE
+        self.mouse_x = x
+        self.mouse_y = y
+    
+    def on_key_release(self, symbol, modifiers):
+        if symbol == pyglet.window.key.LSHIFT and self.panel:
+            self.drop_panel()
+
+    def drop_panel(self):
+        if self.panel:
+            for l in self.panel.label_panel:
+                l.delete()
+            if self.panel.panel_background:
+                self.panel.panel_background.delete()
+                self.panel.panel_background = None
+            self.panel.tags = []
+            self.panel = None
+
+    def set_panel(self, x, y):
+        if not self.panel:
+            self.panel = TagPanel(self, x, y)
+            self.panel.update_labels()
+            
+    def dump_string(self):
+        for k in self.property_index:
+            print(f"{k[0]}, {k[1]}: {', '.join(self.property_index[k])}")
+        print("")
+
+
+class ShortTextInput:
+    
+    def __init__(self, tagger, text, input_x, input_y):
+        self.tagger = tagger
+        self.text = text
+        self.x = input_x
+        self.y = input_y
+        self.tile_x = self.tagger.tile_x
+        self.tile_y = self.tagger.tile_y
+        self.label_item = pyglet.text.Label(text=self.text,
+                                            font_name="Go Mono",
+                                            x=self.x, y=self.y,
+                                            font_size=14, align="left",
+                                            color=(255, 255, 255, 255),
+                                            batch=hover_batch)
+
+    def update_label(self):
+        self.label_item = pyglet.text.Label(text=self.text,
+                                            font_name="Go Mono",
+                                            x=self.x, y=self.y,
+                                            font_size=14,
+                                            align="left",
+                                            color=(255, 255, 255, 255),
+                                            batch=hover_batch)
+
+
+    def on_key_press(self, symbol, modifiers):
+
+        if symbol == pyglet.window.key.ENTER:
+            self.tagger.property_index[self.tile_x, self.tile_y].add(self.label_item.text) 
+            self.label_item.delete()
+            self.label_item = None
+
+        elif symbol in letters and "MOD_SHIFT" in pyglet.window.key.modifiers_string(modifiers):
+            if self.text == ">":
+                self.text = pyglet.window.key.symbol_string(symbol)
+            else:
+                self.text = self.text + pyglet.window.key.symbol_string(symbol)
+                self.label_item.delete()
+                self.update_label()
+
+        elif symbol in letters:
+            if self.text == ">":
+                self.text = pyglet.window.key.symbol_string(symbol).lower()
+            else:
+                self.text = self.text + pyglet.window.key.symbol_string(symbol).lower()
+                self.label_item.delete()
+                self.update_label()
+
+        elif symbol == pyglet.window.key.BACKSPACE and len(self.text) > 0:
+            self.text = self.text[:-1]
+            self.label_item.delete()
+            self.update_label()
+            
+        elif symbol == pyglet.window.key.SPACE:
+            self.text = self.text + ' '
+            self.label_item.delete()
+            self.update_label()
+            
+        elif symbol == pyglet.window.key.PARENLEFT:
+            self.text = self.text + '('
+            self.label_item.delete()
+            self.update_label()
+            
+        elif symbol == pyglet.window.key.PARENRIGHT:
+            self.text = self.text + ')'
+            self.label_item.delete()
+            self.update_label()
+
+        else:
+            pass
+
+class TagPanel:
+    def __init__(self, tagger, x, y):
+        self.tagger = tagger
+        self.tile_x = self.tagger.tile_x
+        self.tile_y = self.tagger.tile_y
+        self.tags = []
+        self.label_panel = []
+        self.x = x
+        self.y = y
+        self.panel_background = None
+
+    def update_tags(self):
+        self.tags = []
+        for t in self.tagger.property_index[self.tile_x, self.tile_y]:
+            self.tags.append(t)
+
+    def update_labels(self):
+        self.update_tags()
+            
+        bottom_y = 0
+        label = None
+
+        for t in self.tags:
+                
+            label = pyglet.text.Label(text=t,
+                                      font_name="Go Mono",
+                                      x=self.x, y=self.y-int(bottom_y),
+                                      font_size=11, align="left",
+                                      color=(255, 255, 255, 220),
+                                      batch=hover_batch)
+                
+            bottom_y += TAG_HEIGHT + TAG_SEPARATOR
+            self.label_panel.append(label)
+
+            bottom_x = 400
+
+            self.panel_background = pyglet.shapes.Rectangle(x=self.x, y=self.y-int(bottom_y)+15, 
+                                                            width=bottom_x,
+                                                            height=bottom_y, 
+                                                            color=(60, 40, 40),
+                                                            batch=foreground_batch)
+
+
+@window.event
+def on_draw():
+    #glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST) 
+    #glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    
+    window.clear()
+    background_batch.draw()
+    foreground_batch.draw()
+    hover_batch.draw()
+    color_batch.draw()
+
+tagger = TileTagger()
+window.push_handlers(tagger)
+
+pyglet.app.run()
