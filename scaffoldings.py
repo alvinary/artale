2ed9fabd8686cd209187528e5d1a8fdc69668018
alvinary <alvaro.cuestas.ll@gmail.com>
2022-04-04 15:53:17 -0300

    Add draft for tree tagger, add old tree scaffolding as binary_tree()
    - The old exponential scaffolding is still useful for types, since
    they are generally small and it avoids storing relations between
    members of the 'type' sort.
    - The draft was tested with a minimal theory (which is probably not
    complete) just to see if everything works and uses a reasonable
    amount of time and memory.
    
    TODO: test with a decent lexicon, check if all the intended properties hold.

--- scaffoldings.py
+++ scaffoldings.py
@@ -7,7 +7,28 @@ def tree(sort_name, size, prefix=""):
         for j in range(i, size):
             facts.add(("before", f"c{i}", f"c{j}"))
     return constants, facts
-    
+
+def binary_tree(sort_name, relations, size, prefix=""):
+    '''Optionally add a prefix to constant names.'''
+
+    tree_constants = set()
+    tree_relation = set()
+
+    root = prefix + sort_name
+
+    rows = [[root]]
+    tree_constants.add(root)
+
+    for index in range(0, size):
+        rows.append([])
+        for tree_elem in rows[index]:
+            for rel in relations:
+                child_name = f"{tree_elem}.{rel}"
+                tree_constants.add(child_name)
+                tree_relation.add((rel, tree_elem, child_name))
+                rows[index + 1].append(child_name)
+
+    return tree_constants, tree_relation
 
 def bounded_state_world(constants, constant_sorts, n):
 
@@ -27,7 +48,7 @@ def bounded_state_world(constants, constant_sorts, n):
                 previous_constant = c + ".{i - 1}"
                 function_map[previous_constant] = fresh_constant
 
-def leveled_relation(sort_name, base_sizes, window_size):
+def leveled_relation(sort_name, base_size, window_size):
 
     new_constants = set()
     new_relations = set()
--- ttag.py
+++ ttag.py
@@ -1,304 +1,91 @@
-from collections import defaultdict
+from parser import Parser
+from models import Relation, Rule, Clause, HornSolver, TERM_SEPARATOR
+from scaffoldings import tree, binary_tree
 
-import pyglet
-from pyglet.gl import glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_TEXTURE_MIN_FILTER, GL_NEAREST
+SIZE_BOUND = 30
 
-SCROLL_SCALING = 40
-TILE_SIDE = 48
-SCROLLABLE_PANEL_TOP = 400
-TAG_SEPARATOR = 8
-TAG_HEIGHT = 16
+solver = HornSolver()
 
-letters = [
-    l.upper()
-    for l in "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
-]
+theory_program = ""
 
+with open("./specs/theory") as theory_file:
+    for line in theory_file:
+        theory_program = theory_program + line
 
-def as_ascii(s):
-    return pyglet.window.key.symbol_string(s)
+sort_declarations, rule_declarations = Parser().parse(theory_program)
 
+for rule in rule_declarations:
+    body, head, variables, sorts, flags = rule
+    solver.rules.append(Rule([Relation([term for term in atom]) for atom in head],
+                             [Relation([term for term in atom]) for atom in body],
+                             [s for v, s in variables], [v for v, s in variables], {}, flags))
 
-cool_purple = (58, 58, 165)
-cool_coral = (192, 64, 64)
-cool_white = (248, 231, 231)
+print("Assembling tree...")
 
-window = pyglet.window.Window(1920, 1080)
+tree_constants, tree_facts = tree("node", SIZE_BOUND)
 
-background_batch = pyglet.graphics.Batch()
-foreground_batch = pyglet.graphics.Batch()
-hover_batch = pyglet.graphics.Batch()
+solver.sorts["node"] = list(tree_constants)
 
-color_batch = pyglet.graphics.Batch()
+for f in tree_facts:
+    tree_clause = Clause(f, [])
+    solver.update_maps([tree_clause])
+    solver.solver.add_clause(solver.dimacs(tree_clause))
 
-index = lambda: defaultdict(lambda: set())
+for t in tree_constants:
+    print(f"tree for {t}...")
+    node_drs = f"{t}.drs"
+    node_type = f"{t}.type"
+    
+    solver.value_map[t, "drs"] = node_drs
+    solver.value_map[t, "type"] = node_type
 
-tileset_resource = pyglet.image.load("./images/61816.png")
+    solver.sorts["type"].append(node_type)
+    solver.sorts["drs"].append(node_drs)
 
+    for semitem in ["self", "1", "2", "3", "4"]:
+        solver.value_map[t, semitem] = f"{t}.{semitem}"
+        solver.sorts["semitem"].append(f"{t}.{semitem}")
 
-class TileTagger:
+    for i in range(1, 3):
+        solver.value_map[f"{t}.{str(i)}", "next"] = f"{t}.{str(i+1)}"
+        solver.value_map[f"{t}.{str(i+1)}", "previous"] = f"{t}.{str(i)}"
+    
+    solver.value_map[f"{t}.4", "next"] = "blanksemitem"
+    solver.value_map[f"{t}.1", "previous"] = "blanksemitem"
 
-    def __init__(self):
-        self.properties = set()
-        self.tile_index = index()  # tile_index[a, b] = tile_name
-        self.property_index = index()
-        self.tilemap = pyglet.sprite.Sprite(tileset_resource,
-                                            x=0,
-                                            y=0,
-                                            batch=background_batch)
-        self.panel = None
-        self.label = None
-        self.tile_x = 0
-        self.tile_y = 0
-        self.scroll_shift_y = 0
-        self.scroll_shift_x = 0
+    solver.value_map[node_drs, "self"] = f"{node_drs}.self"
 
-        self.tilemap.scale_x = 3
-        self.tilemap.scale_y = 3
+    type_constants, type_tree = binary_tree("", ["input", "output"], 4, prefix=node_type)
 
-        self.drag = False
+    solver.sorts["type"] = solver.sorts["type"] + list(type_constants)
+    
+    chomeur = set(type_constants)
 
-    def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
-        self.tilemap.y = self.tilemap.y - int(scroll_y * SCROLL_SCALING)
-        self.scroll_shift_y -= int(scroll_y * SCROLL_SCALING)
-
-    def on_key_press(self, symbol, modifiers):
-
-        if symbol == pyglet.window.key.LSHIFT:
-            if self.property_index[self.tile_x, self.tile_y]:
-                self.set_panel(self.mouse_x, self.mouse_y)
-
-        if symbol == pyglet.window.key.TAB:
-            self.dump_string()
-
-        if symbol == pyglet.window.key.LEFT:
-            self.scroll_shift_x += 6 * TILE_SIDE
-            self.tilemap.x += 6 * TILE_SIDE
-
-        if symbol == pyglet.window.key.RIGHT:
-            self.scroll_shift_x -= 6 * TILE_SIDE
-            self.tilemap.x -= 6 * TILE_SIDE
-
-        if symbol == pyglet.window.key.UP:
-            self.scroll_shift_y -= 6 * TILE_SIDE
-            self.tilemap.y -= 6 * TILE_SIDE
-
-        if symbol == pyglet.window.key.DOWN:
-            self.scroll_shift_y += 6 * TILE_SIDE
-            self.tilemap.y += 6 * TILE_SIDE
-
-    def on_mouse_press(self, x, y, button, modifiers):
-
-        if button == pyglet.window.mouse.LEFT:
-
-            if self.label:
-                if self.label.label_item:
-                    self.label.label_item.delete()
-                self.label = None
-
-            self.label = ShortTextInput(self, ">", x, y)
-            window.push_handlers(self.label)
-
-        if button == pyglet.window.mouse.RIGHT:
-            self.drag = not self.drag
-
-    def on_mouse_motion(self, x, y, dx, dy):
-        self.tile_x = (x - self.scroll_shift_x) // TILE_SIDE
-        self.tile_y = (y - self.scroll_shift_y) // TILE_SIDE
-        self.mouse_x = x
-        self.mouse_y = y
-
-        if self.drag:
-            self.scroll_shift_y += dy
-            self.scroll_shift_x += dx
-            self.tilemap.x += dx
-            self.tilemap.y += dy
-
-    def on_key_release(self, symbol, modifiers):
-        if symbol == pyglet.window.key.LSHIFT and self.panel:
-            self.drop_panel()
-
-    def drop_panel(self):
-        if self.panel:
-            for l in self.panel.label_panel:
-                l.delete()
-            if self.panel.panel_background:
-                self.panel.panel_background.delete()
-                self.panel.panel_background = None
-            self.panel.tags = []
-            self.panel = None
-
-    def set_panel(self, x, y):
-        if not self.panel:
-            self.panel = TagPanel(self, x, y)
-            self.panel.update_labels()
-
-    def dump_string(self):
-        for k in self.property_index:
-            print(f"{k[0]}, {k[1]}: {', '.join(self.property_index[k])}")
-        print("")
-
-    def load_string(self, text):
-        for line in text.split("\n"):
-            coordinates = tuple(line.split(":")[0].strip().split(", "))
-            properties = {p.strip() for p in line.split(":")[1].split(",")}
-            self.property_index[coordinates] |= properties
-
-
-class ShortTextInput:
-
-    def __init__(self, tagger, text, input_x, input_y):
-        self.tagger = tagger
-        self.text = text
-        self.x = input_x
-        self.y = input_y
-        self.tile_x = self.tagger.tile_x
-        self.tile_y = self.tagger.tile_y
-        self.label_item = pyglet.text.Label(text=self.text,
-                                            font_name="Go Mono",
-                                            x=self.x,
-                                            y=self.y,
-                                            font_size=14,
-                                            align="left",
-                                            color=(255, 255, 255, 255),
-                                            batch=hover_batch)
-
-    def update_label(self):
-        self.label_item = pyglet.text.Label(text=self.text,
-                                            font_name="Go Mono",
-                                            x=self.x,
-                                            y=self.y,
-                                            font_size=14,
-                                            align="left",
-                                            color=(255, 255, 255, 255),
-                                            batch=hover_batch)
-
-    def on_key_press(self, symbol, modifiers):
-
-        if symbol == pyglet.window.key.ENTER:
-        
-            new_tags = set([s.strip() for s in self.text.split(",")])
-
-            self.tagger.property_index[self.tile_x,
-                                           self.tile_y] |= new_tags
-            self.label_item.delete()
-            self.label_item = None
-
-        elif as_ascii(
-                symbol
-        ) in letters and "MOD_SHIFT" in pyglet.window.key.modifiers_string(
-                modifiers):
-            if self.text == ">":
-                self.text = pyglet.window.key.symbol_string(symbol)
-            else:
-                self.text = self.text + pyglet.window.key.symbol_string(symbol)
-                self.label_item.delete()
-                self.update_label()
-
-        elif as_ascii(symbol) in letters:
-            if self.text == ">":
-                self.text = pyglet.window.key.symbol_string(symbol).lower()
-            else:
-                self.text = self.text + pyglet.window.key.symbol_string(
-                    symbol).lower()
-                self.label_item.delete()
-                self.update_label()
-
-        elif symbol == pyglet.window.key.BACKSPACE and len(self.text) > 0:
-            self.text = self.text[:-1]
-            self.label_item.delete()
-            self.update_label()
-
-        elif symbol == pyglet.window.key.SPACE:
-            self.text = self.text + ' '
-            self.label_item.delete()
-            self.update_label()
-
-        elif symbol == pyglet.window.key.PARENLEFT:
-            self.text = self.text + '('
-            self.label_item.delete()
-            self.update_label()
-
-        elif symbol == pyglet.window.key.PARENRIGHT:
-            self.text = self.text + ')'
-            self.label_item.delete()
-            self.update_label()
-
-        elif symbol == pyglet.window.key.COLON:
-            self.text = self.text + ":"
-            self.label_item.delete()
-            self.update_label()
-
-        elif symbol == pyglet.window.key.COMMA:
-            self.text = self.text + ","
-            self.label_item.delete()
-            self.update_label()
-
-        else:
-            pass
-
-
-class TagPanel:
-
-    def __init__(self, tagger, x, y):
-        self.tagger = tagger
-        self.tile_x = self.tagger.tile_x
-        self.tile_y = self.tagger.tile_y
-        self.tags = []
-        self.label_panel = []
-        self.x = x
-        self.y = y
-        self.panel_background = None
-
-    def update_tags(self):
-        self.tags = []
-        for t in self.tagger.property_index[self.tile_x, self.tile_y]:
-            self.tags.append(t)
-
-    def update_labels(self):
-        self.update_tags()
-
-        bottom_y = 0
-        label = None
-
-        for t in self.tags:
-
-            label = pyglet.text.Label(text=t,
-                                      font_name="Go Mono",
-                                      x=self.x,
-                                      y=self.y - int(bottom_y),
-                                      font_size=11,
-                                      align="left",
-                                      color=(255, 255, 255, 220),
-                                      batch=hover_batch)
-
-            bottom_y += TAG_HEIGHT + TAG_SEPARATOR
-            self.label_panel.append(label)
-
-            bottom_x = 400
-
-            self.panel_background = pyglet.shapes.Rectangle(
-                x=self.x,
-                y=self.y - int(bottom_y) + 15,
-                width=bottom_x,
-                height=bottom_y,
-                color=(60, 40, 40),
-                batch=foreground_batch)
-
-
-@window.event
-def on_draw():
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-
-    window.clear()
-    background_batch.draw()
-    foreground_batch.draw()
-    hover_batch.draw()
-    color_batch.draw()
-
-
-tagger = TileTagger()
-window.push_handlers(tagger)
-
-pyglet.app.run()
+    for f, k, v in type_tree:
+        solver.value_map[k, f] = v
+        chomeur.discard(k)
+
+    for k in chomeur:
+        for f in ["input", "output"]:
+            solver.value_map[k, f] = "blanktype"
+
+print("Done")
+
+print("Unfolding...")
+
+solver.unfold_instance()
+solver.una_equality()
+
+print("Done")
+
+print("Looking for models...")
+
+for i in range(800, 911):
+    print(i)
+    res = solver.solver.solve([i])
+    if res:
+        print(":D :D :D")
+        m = solver.solver.get_model()
+        print(len(m), "", len([a for a in m if a > 0]))
+    else:
+        print(f"D:")
