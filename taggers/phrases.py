c8b05f977c0b9f19b4615356375ded28bd00a400
alvinary <alvaro.cuestas.ll@gmail.com>
2022-07-18 10:05:50 -0300

    Add missing 'next' assertions

--- taggers/phrases.py
+++ taggers/phrases.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from artale.constants import *
 from artale.parser import read_into, normalize
 from artale.models import Relation, Rule, Clause, HornSolver, TERM_SEPARATOR
@@ -17,16 +19,27 @@ readable_model = solver.show_model(model)
 print(readable_model)
 
 nodes = solver.sorts["node"]
+node_indices = [i + 1 for i, n in enumerate(nodes)]
 
 root_is_phrase = "phrase node1"
-next_is_next = [f"next node{i + 1} node{i + 2}" for i in range(len(nodes))]
+
+nexts = []
+   
+make_next = lambda x : f"next node{x} node{x+1}"
+make_not_next = lambda i, j : f"not next node{i} node{j}"
+
+index_pairs = product(node_indices, node_indices)
+not_nexts = [make_not_next(i, j) for i, j in index_pairs if i + 1 != j]
+nexts = [make_next(i) for i in node_indices]
+nexts = nexts + not_nexts
 
 not_befores = [f"not before {n1} {n2}" for n1 in nodes for n2 in nodes if n1 >= n2]
 befores = [f"before {n1} {n2}" for n1 in nodes for n2 in nodes if n1 < n2]
+
 rights = [f"right {n1} {n2}" for n1 in nodes for n2 in nodes if n1 < n2]
 lefts = [f"left {n1} {n2}" for n1 in nodes for n2 in nodes if n1 < n2]
 
-additional_facts = [root_is_phrase] + not_befores + befores
+additional_facts = [root_is_phrase] + not_befores + befores + nexts
 
 for f in additional_facts:
     solver.add_assertion(f)
