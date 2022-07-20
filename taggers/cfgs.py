0eebb7c75658d87d5faa7a477d690b7f96467b47
alvinary <alvaro.cuestas.ll@gmail.com>
2022-07-20 14:56:01 -0300

    Add missing string formatting calls, add tree embedding logic

--- taggers/cfgs.py
+++ taggers/cfgs.py
@@ -1,6 +1,8 @@
+from itertools import product
+
 from artale.models import HornSolver
 from artale.parser import read_into
-from artale.specs import cfg
+from artale.specs import cfg, trees
 
 STRINGS = "S"
 RULES = "rule"
@@ -10,26 +12,76 @@ NEXT = "next"
 POSITIVE = "pos"
 NEGATIVE = "neg"
 VOID = "void"
+NODES = "node"
+
+NUMBER_OF_PRETERMINALS = 6
 
 # Define artale.n_models(n, rels=[], program)
 
+# Embed a tree
+
+def make_tree(size, name, solver):
+
+    new_constants = {}
+    new_constants[NODES] = []
+
+    for i in range(size):
+        new_node = f"node:{name}:{i+1}"
+        new_constants[NODES].append(new_node)
+        
+    nodes = new_constants[NODES]
+    node_indices = [i for i, n in enumerate(nodes)]
+        
+    make_next = lambda x : f"next node:{name}:{x} node:{name}:{x+1}"
+    make_not_next = lambda i, j : f"not next node:{name}:{i} node:{name}:{j}"
+
+    index_pairs = product(node_indices, repeat=2)
+    node_pairs = lambda : product(nodes, repeat=2)
+    not_nexts = [make_not_next(i, j) for i, j in index_pairs if i + 1 != j]
+    nexts = [make_next(i) for i in node_indices]
+    nexts = nexts + not_nexts
+
+    not_befores = [f"not before {n1} {n2}" for n1, n2 in node_pairs() if n1 >= n2]
+    befores = [f"before {n1} {n2}" for n1, n2 in node_pairs() if n1 < n2]
+
+    rights = [f"right {n1} {n2}" for n1, n2 in node_pairs() if n1 < n2]
+    lefts = [f"left {n1} {n2}" for n1, n2 in node_pairs() if n1 < n2]
+    
+    root_is_phrase = f"has root {name} node:{name}:1"
+
+    additional_facts = [root_is_phrase] + not_befores + befores + nexts
+    
+    for fact in additional_facts:
+        solver.add_assertion(fact)
+        
+    return new_constants
+    
 # Embed a string
 
 def instancify(string, name, solver):
     chars = set()
     next_to_last = len(string) - 1
     
-    new_constants = []
+    new_constants = {}
+    new_constants[NODES] = []
+    new_constants[STRINGS] = []
     
     if not name:
         name = string
     for i, c in enumerate(string):
         new_string = f"{name}:{i+1}"
         char_assertion = f"is {new_string} {c}"
+        remainder = len(string) - i
+        before_assertions = [f"before {new_string} {name}:{i+k}" for k in range(0, remainder)]
+        after_assertions = [f"not before {name}:{i+k} {new_string}" for k in range(1, i)]
+        
         solver.add_assertion(char_assertion)
+        for b in before_assertions:
+            solver.add_assertion(b)
+        
         chars.add(c)
         solver.add_element(STRINGS, new_string)
-        new_constants.append(new_string)
+        new_constants[STRINGS].append(new_string)
         if i < next_to_last:
             next_string = f"{name}:{i+2}"
             solver.value_map[NEXT, new_string] = next_string
@@ -39,7 +91,7 @@ def instancify(string, name, solver):
     for c in chars:
         if c not in solver.sorts[CHARS]:
             solver.add_element(CHARS, c)
-    
+        
     return new_constants
     
 def make_instance(pos, neg):
@@ -47,11 +99,12 @@ def make_instance(pos, neg):
     solver = HornSolver()
     solver.verbose = True
     read_into(cfg, solver)
-    solver.fill_sort("T", 8)
+    read_into(trees, solver)
+    solver.fill_sort("T", NUMBER_OF_PRETERMINALS)
     solver.add_element(PRETERMINALS, "start")
     
-    is_local = lambda x : STRINGS in x.sorts
-    is_global = lambda x : STRINGS not in x.sorts
+    is_local = lambda x : STRINGS in x.sorts or NODES in x.sorts
+    is_global = lambda x : STRINGS not in x.sorts and NODES not in x.sorts
     
     local_rules = [r for r in solver.rules if is_local(r)]
     global_rules = [r for r in solver.rules if is_global(r)]
@@ -71,15 +124,21 @@ def make_instance(pos, neg):
     
     local_sorts = {}
 
-    for i, p in enumerate(pos):
+    for i, string in enumerate(pos):
         positive_example = f"p{i}"
-        local_sorts[positive_example] = instancify(p, positive_example, solver)
+        tree_size = len(string) - 1
+        new_constants = instancify(string, positive_example, solver)
+        new_constants |= make_tree(tree_size, positive_example, solver)
+        local_sorts[positive_example] = new_constants
         solver.add_element(POSITIVE, positive_example)
         
-    for i, n in enumerate(neg):
+    for i, string in enumerate(neg):
         negative_example = f"n{i}"
-        local_sorts[negative_example] = instancify(n, negative_example, solver)
-        solver.add_element(NEGATIVE, negative_example)
+        tree_size = len(string) - 1
+        new_constants = instancify(string, negative_example, solver)
+        new_constants |= make_tree(tree_size, negative_example, solver)
+        local_sorts[negative_example] = new_constants
+        solver.add_element(NEGATIVE, positive_example)
         
     for k in solver.value_map.keys():
         print(f"{str(k)} : {solver.value_map[k]}")
@@ -91,8 +150,7 @@ def make_instance(pos, neg):
         
     for local_rule in local_rules:
         for string in local_sorts.keys():
-            local_restrictions = {}
-            local_restrictions[STRINGS] = local_sorts[string]
+            local_restrictions = local_sorts[string]
             solver.unfold_rule(local_rule, local_restrictions)
             
     solver.unfold_una()
@@ -110,4 +168,4 @@ def make_instance(pos, neg):
         
 paren_pos = ["(a+b)+b", "(a+(b+c))", "((a+a)+b)+c", "(a+((c+b)+(a +((c+a)+b)))", "a+((b+c)+b)"]
 paren_neg = ["(a+b+c)", "(a)+)b", "((a+)c(", "(a)+(b+)+(c+c)"]
-make_instance(["aa", "aaaa"], ["a", "aaa"])
+make_instance(paren_pos, paren_neg)
